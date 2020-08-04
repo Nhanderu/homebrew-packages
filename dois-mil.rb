0c844d7706644308bb783dccfb0b732e545cf4e7
Rafael Escobar <rescobar@redventures.com>
2020-08-04 20:33:16 -0300

    Update dois-mil version

--- dois-mil.rb
+++ dois-mil.rb
@@ -1,17 +1,17 @@
 class DoisMil < Formula
-    desc "2048 in the terminal!"
-    homepage "https://github.com/Nhanderu/dois-mil"
-    url "https://github.com/Nhanderu/dois-mil/archive/v1.0.0.tar.gz"
-    sha256 "a96274be0f33cd3dd2d812436750ce5cb087e7cd860ef893a8d49df309e3c853"
-  
-    depends_on "rust" => :build
-  
-    def install
-      system "cargo", "install", "--locked", "--root", prefix, "--path", "."
-    end
-  
-    test do
-      system "#{bin}/dois-mil", "version"
-    end
+  desc "2048 in the terminal!"
+  homepage "https://github.com/Nhanderu/dois-mil"
+  url "https://github.com/Nhanderu/dois-mil/archive/v1.0.1.tar.gz"
+  sha256 "cc51a84f061600c9157e5216fbb6da2eae81ae0cfe2750ead3d4da01d452bc05"
+  license "Unlicense"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
   end
-  
+
+  test do
+    system "#{bin}/dois-mil", "version"
+  end
+end
