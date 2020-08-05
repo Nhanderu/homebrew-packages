2cd24eb6502aeed2c187e2ced43e0a263baa73c0
Rafael Escobar <rescobar@redventures.com>
2020-08-05 11:51:22 -0300

    Update dioni version

--- dioni.rb
+++ dioni.rb
@@ -1,8 +1,8 @@
 class Dioni < Formula
   desc "Real random for Spotify!"
   homepage "https://github.com/Nhanderu/dioni"
-  url "https://github.com/Nhanderu/dioni/archive/v1.0.0.tar.gz"
-  sha256 "37a61138151b58636032a13dbc75c9ddab1dd026e49e63cad81a0a54ccfc4ae2"
+  url "https://github.com/Nhanderu/dioni/archive/v1.0.1.tar.gz"
+  sha256 "3a77dd2400d73036199e25ed92be333c6d756324d4bc1420b37e2938588a649a"
   license "Unlicense"
 
   depends_on "rust" => :build
