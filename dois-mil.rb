350c26a1ce593d703eb5968e177a21ebb435fbe3
Rafael Escobar <rescobar@redventures.com>
2020-08-04 21:48:58 -0300

    Update dois-mil version

--- dois-mil.rb
+++ dois-mil.rb
@@ -1,8 +1,8 @@
 class DoisMil < Formula
   desc "2048 in the terminal!"
   homepage "https://github.com/Nhanderu/dois-mil"
-  url "https://github.com/Nhanderu/dois-mil/archive/v1.0.1.tar.gz"
-  sha256 "cc51a84f061600c9157e5216fbb6da2eae81ae0cfe2750ead3d4da01d452bc05"
+  url "https://github.com/Nhanderu/dois-mil/archive/v1.0.2.tar.gz"
+  sha256 "83c38ef42fa0b25834d9efc2815d7dcce5632aebd7448dd774d22f76cef61d48"
   license "Unlicense"
 
   depends_on "rust" => :build
