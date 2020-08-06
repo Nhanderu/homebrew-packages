8f9ddf05c78419943a89832bb812d30fb577444c
Rafael Escobar <rescobar@redventures.com>
2020-08-06 16:49:28 -0300

    Update gridt

--- gridt.rb
+++ gridt.rb
@@ -1,8 +1,8 @@
 class Gridt < Formula
   desc "Display contents as a grid in the terminal!"
   homepage "https://github.com/Nhanderu/gridt"
-  url "https://github.com/Nhanderu/gridt/archive/v2.0.0.tar.gz"
-  sha256 "58ed295162c9c06dcfc54cd21ec725819e94b6ceaf427049398677ff75605794"
+  url "https://github.com/Nhanderu/gridt/archive/v2.0.1.tar.gz"
+  sha256 "4d71a9c4a7562bc342ec1541611203200acf29d6e309227580c54ccad7df51e2"
   license "Unlicense"
 
   depends_on "go" => :build
